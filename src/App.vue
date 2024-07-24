aae4883f0763babe9dba7b2a6ff6c015d7fb9c11
furkan.turgut@turkuvazinovasyon.com <furkan.turgut@turkuvazinovasyon.com>
2024-07-24 23:13:14 +0300

    fffff

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-5 bg-blue-100 border-round-xl m-5 flex flex-column justify-content-start align-content-center h-screen">
+    <DataTable :value="customers">
+      <Column field="customerName" header="Müşteri adı"></Column>
+      <Column field="customerEmail" header="Müşteri e-mail adresi"></Column>
+      <Column field="customerPhone" header="Müşteri telefonu"></Column>
+    </DataTable>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+import { getFirestore, query,collection,getDocs } from 'firebase/firestore';
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: 'CustomerManagement',
+  setup() {
+    const customerList = ref([]);
+    const db = getFirestore();
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    const getCustomerData = async () => {
+      const q = query(collection(db, "customers"));
+      await getDocs(q).then((querySnapshot) => {
+        querySnapshot.forEach((item) => {
+          customerList.value.push(item.data());
+          console.log(customerList.value)
+        });
+      });
+
+    }
+    onMounted(() => {
+      getCustomerData();
+    })
+
+    return {
+      customerList,
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyBiDBG_AUCI6z3NpMf2V8IOflAau_SJRD4",
+    authDomain: "fatura-kesme-ve-yonetme.firebaseapp.com",
+    projectId: "fatura-kesme-ve-yonetme",
+    storageBucket: "fatura-kesme-ve-yonetme.appspot.com",
+    messagingSenderId: "1035428225343",
+    appId: "1:1035428225343:web:d8c773c60ad1c9482b338a",
+    measurementId: "G-M16R0XN9RS"
+};
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp)
 
-createApp(App).mount('#app')
+export{db}
+const app = createApp(App)
+app.mount('#app')
